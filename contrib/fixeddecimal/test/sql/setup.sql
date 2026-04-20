4470d46b300a061a504b39337b3f9c15470d0459
Dianjin Wang <wangdianjin@gmail.com>
2026-04-20 12:04:18 +0800

    contrib/fixeddecimal: install index support by default
    Make CREATE EXTENSION fixeddecimal install the advertised index support again.
    
    Re-add fixeddecimal--opclass.sql and fixeddecimal--brin.sql to the generated extension script so btree, hash, and BRIN operator classes are installed with the extension by default.
    
    Keep the setup regression, but change it from explicit script loading to catalog verification that the extension-installed operator classes already exist.

--- contrib/fixeddecimal/test/sql/setup.sql
+++ contrib/fixeddecimal/test/sql/setup.sql
@@ -1,3 +1,16 @@
-\i fixeddecimal--brin.sql
-
-\i fixeddecimal--opclass.sql
+SELECT am.amname, opc.opcname
+FROM pg_opclass opc
+JOIN pg_am am ON am.oid = opc.opcmethod
+JOIN pg_namespace nsp ON nsp.oid = opc.opcnamespace
+WHERE nsp.nspname = current_schema()
+  AND opc.opcname IN (
+    'fixeddecimal_minmax_ops',
+    'fixeddecimal_ops',
+    'fixeddecimal_numeric_ops',
+    'numeric_fixeddecimal_ops',
+    'fixeddecimal_int4_ops',
+    'int4_fixeddecimal_ops',
+    'fixeddecimal_int2_ops',
+    'int2_fixeddecimal_ops'
+  )
+ORDER BY am.amname, opc.opcname;
