a1adbe25bb14adfddbaae397f9299a048380580a
Dianjin Wang <wangdianjin@gmail.com>
2026-06-26 18:27:12 +0800

    update

--- gpMgmt/test/behave/mgmt_utils/steps/mgmt_utils.py
+++ gpMgmt/test/behave/mgmt_utils/steps/mgmt_utils.py
@@ -4086,19 +4086,18 @@ def impl(context):
     for host in host_to_pid_map:
         for pid in host_to_pid_map[host]:
             # gpstop/gpstart can return before every saved pid fully exits.
-            # Poll briefly to avoid flaking on processes that are
-            # already shutting down.
-            for _ in range(10):
+            # Poll to avoid flaking on processes that are already shutting down.
+            for _ in range(60):
                 if not unix.check_pid_on_remotehost(pid, host):
                     break
                 time.sleep(1)
             else:
-                # Process still alive after 10s; try forceful kill
+                # Process still alive after 60s; try forceful kill
                 # then re-check.
                 cmd = Command("kill process", "kill -9 %d" % pid,
                               ctxt=REMOTE, remoteHost=host)
                 cmd.run()
-                for _ in range(30):
+                for _ in range(10):
                     if not unix.check_pid_on_remotehost(pid, host):
                         break
                     time.sleep(1)
